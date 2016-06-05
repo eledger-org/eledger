c7b335522a123fa9b78a8eceb372d0da2d400dbd
Henry <hankbrown@gmail.com>
2016-06-05 15:21:44 -0700

    Cleaned up the GUI with a partially transparent overlay.

--- static/js/file-upload.js
+++ static/js/file-upload.js
@@ -1,16 +1,19 @@
 function set_body_height() {
+  var ww = $(window).width();
   var wh = $(window).height();
 
   $('#overlay-image').attr('style', 'height:' + wh + 'px;');
 }
 
 $(document).ready(function() {
+  $('#overlay-tint').hide();
   set_body_height();
 
   $(window).bind('resize', function() { set_body_height(); });
 
-  $('#container').click(function() {
+  $('#overlay-tint').click(function() {
     $('#overlay-image').hide();
+    $('#overlay-tint').hide();
   });
 
   $('.display-image-button').click(function() {
@@ -18,6 +21,9 @@ $(document).ready(function() {
       url: "/api/get-file/" + $(this).attr('filename'),
       async: true,
       success: function(responseText) {
+        $('#overlay-tint').attr("src", responseText);
+        $('#overlay-tint').zIndex = 254;
+        $('#overlay-tint').show();
         $('#overlay-image').attr("src", responseText);
         $('#overlay-image').zIndex = 255;
         $('#overlay-image').show();
