b3a541df47d052bdf2d62a479bf2e911b33a9c86
Henry <hankbrown@gmail.com>
2016-06-05 12:15:53 -0700

    Renamed FileController to FilesController for consistency.

--- router.js
+++ router.js
@@ -1,9 +1,9 @@
 var HomeController = require('./controllers/HomeController');
-var FileController = require('./controllers/FileController');
+var FilesController = require('./controllers/FilesController');
 
 module.exports = function(app) {
   app.get('/', HomeController.Index);
   app.get('/other', HomeController.Other);
-  app.get('/file-upload', FileController.Index);
-  app.get('/api/get-file/:id', FileController.GetFile);
+  app.get('/file-upload', FilesController.Index);
+  app.get('/api/get-file/:id', FilesController.GetFile);
 };
