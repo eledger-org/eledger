8ed9c0a857f2fe747f9b4912dce05387011dcba6
Henry <hankbrown@gmail.com>
2016-06-06 01:41:09 -0700

    Added body parser for parsing json and added a helper jsonpp function for pretty printing json inside of the handlebars views.

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ var express       = require('express');
 var http          = require('http');
 var path          = require('path');
 var hbs           = require('hbs');
+var bodyParser    = require('body-parser');
 
 var app = express();
 
@@ -29,14 +30,22 @@ console.log("=".repeat(process.stdout.columns));
 require('./router')(app);
 
 hbs.registerPartials(__dirname + "/views/templates");
+
 hbs.registerHelper('json', function(obj) {
   return JSON.stringify(obj);
+});
+
+hbs.registerHelper('jsonpp', function(obj) {
+  return JSON.stringify(obj, null, 2);
 });
 
 app.set('view options', { layout: 'layouts/main' });
 app.set('views', path.join(__dirname, 'views'));
 
 app.set('view engine', 'hbs');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use('/static/uploads', express.static('uploads'));
 app.use('/static/bootstrap', express.static('bootstrap'));
