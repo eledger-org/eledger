72b183029ecb9d84988ab47667d45dc95cd97732
Henry <hankbrown@gmail.com>
2016-06-17 20:32:39 -0700

    Fixed linting errors index.js

--- index.js
+++ index.js
@@ -1,54 +1,53 @@
-var config        = require('config');
-var express       = require('express');
-var http          = require('http');
-var path          = require('path');
-var hbs           = require('hbs');
-var bodyParser    = require('body-parser');
+var config        = require("config");
+var express       = require("express");
+var path          = require("path");
+var hbs           = require("hbs");
+var bodyParser    = require("body-parser");
 
 var app = express();
 
-var Log = require('node-android-logging');
+var Log = require("node-android-logging");
 
-console.log("=".repeat(process.stdout.columns));
+Log.I("=".repeat(process.stdout.columns));
 
 // Initialize all the models.
-require('./models/model');
+require("./models/model");
 
 Log.I("Finished initializing the database.");
 
-console.log("=".repeat(process.stdout.columns));
+Log.I("=".repeat(process.stdout.columns));
 
-require('./router')(app);
+require("./router")(app);
 
 hbs.registerPartials(__dirname + "/views/templates");
 
-hbs.registerHelper('json', function(obj) {
+hbs.registerHelper("json", function(obj) {
   return JSON.stringify(obj);
 });
 
-hbs.registerHelper('jsonpp', function(obj) {
+hbs.registerHelper("jsonpp", function(obj) {
   return JSON.stringify(obj, null, 2);
 });
 
-app.set('view options', { layout: 'layouts/main' });
-app.set('views', path.join(__dirname, 'views'));
+app.set("view options", { layout: "layouts/main" });
+app.set("views", path.join(__dirname, "views"));
 
-app.set('view engine', 'hbs');
+app.set("view engine", "hbs");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use('/static/uploads', express.static('uploads'));
-app.use('/static/bootstrap', express.static('bootstrap'));
-app.use('/static', express.static('static'));
-app.use('/ocr', express.static('/tmp/ocr'));
+app.use("/static/uploads", express.static("uploads"));
+app.use("/static/bootstrap", express.static("bootstrap"));
+app.use("/static", express.static("static"));
+app.use("/ocr", express.static("/tmp/ocr"));
 
-var server = app.listen(config.get('express.port'), function() {
+var server = app.listen(config.get("express.port"), function() {
   var port = server.address().port;
 
   Log.I("Listening on port %s", port);
   Log.I("Server initialized.");
 
-  console.log("=".repeat(process.stdout.columns));
+  Log.I("=".repeat(process.stdout.columns));
 });
 
