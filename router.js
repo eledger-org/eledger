3ded5bb842b3ea2849cac7a79ffd4c8fb7201d21
Henry <hankbrown@gmail.com>
2016-06-17 20:32:58 -0700

    Removed HomeController

--- router.js
+++ router.js
@@ -1,12 +1,9 @@
-var HomeController = require('./controllers/HomeController');
 var FilesController = require('./controllers/FilesController');
 var ProofsController = require('./controllers/ProofsController');
 
 var jsonParser = require('body-parser').json();
 
 module.exports = function(app) {
-  app.get('/', HomeController.Index);
-  app.get('/other', HomeController.Other);
   app.get('/file-upload', FilesController.Index);
   app.get('/proofs', ProofsController.Index);
   app.get('/proofs/:id', ProofsController.ProofById);
