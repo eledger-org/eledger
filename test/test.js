d98f4feb2f656de56d2e3379ccf703ece4285a2c
Henry <hankbrown@gmail.com>
2016-06-17 20:55:03 -0700

    Added strict mode, Fixed linting errors test/test.js

--- test/test.js
+++ test/test.js
@@ -1,9 +1,11 @@
+"use strict";
+
 /* eslint-env mocha */
 
 var assert = require("chai").assert;
 
 describe("Log", function() {
-  it('should assert 1 === 1', function() {
+  it("should assert 1 === 1", function() {
     assert.equal(1, 1);
   });
 });
