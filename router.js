ba793a2d1005c56a47864b0e438166fc16239aa2
Henry <hankbrown@gmail.com>
2016-06-17 20:48:21 -0700

    Added strict mode, Fixed linting errors router.js

--- router.js
+++ router.js
@@ -1,13 +1,15 @@
-var FilesController = require('./controllers/FilesController');
-var ProofsController = require('./controllers/ProofsController');
+"use strict";
 
-var jsonParser = require('body-parser').json();
+var FilesController = require("./controllers/FilesController");
+var ProofsController = require("./controllers/ProofsController");
+
+var jsonParser = require("body-parser").json();
 
 module.exports = function(app) {
-  app.get('/file-upload', FilesController.Index);
-  app.get('/proofs', ProofsController.Index);
-  app.get('/proofs/:id', ProofsController.ProofById);
-  app.get('/api/proofs/next', ProofsController.NextProof);
-  app.put('/api/proofs/:id', jsonParser, ProofsController.SaveById);
-  app.get('/api/get-file/:id', FilesController.GetFile);
+  app.get("/file-upload", FilesController.Index);
+  app.get("/proofs", ProofsController.Index);
+  app.get("/proofs/:id", ProofsController.ProofById);
+  app.get("/api/proofs/next", ProofsController.NextProof);
+  app.put("/api/proofs/:id", jsonParser, ProofsController.SaveById);
+  app.get("/api/get-file/:id", FilesController.GetFile);
 };
